921d45172ed36da857edcfa4a37693177d5fc048
Hantasmagoria <asm.flux@gmail.com>
2022-07-15 14:26:25 +0800

    updated Header with two new buttons: Filter and Refresh

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {BiFilterAlt, BiRefresh} from 'react-icons/bi';
+import Button from './Button';
 
 const Header = ({ category, title }) => (
   <div className=" mb-10">
     <p className="text-lg text-gray-400">{category}</p>
     <p className="text-3xl font-extrabold tracking-tight text-slate-50">
       {title}
+      <div style={{float:"right" }}><Button icon={<BiFilterAlt />} bgColor="#46566c" color="#fff" borderRadius="10px" />
+      <Button icon={<BiRefresh />} bgColor="#46566c" color="#fff" borderRadius="10px" flip="180" /></div>
     </p>
   </div>
 );
